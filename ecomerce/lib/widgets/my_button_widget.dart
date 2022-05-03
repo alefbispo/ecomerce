7c424b59424d9a527b230aebdcc7ae172831d0c3
alefbispo <Blacksabbath21@>
2022-05-03 20:18:55 -0300

    botao generico para usar no app

---
+++ ecomerce/lib/widgets/my_button_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyButtonWidget extends StatelessWidget {
+  final String text;
+  final Color color;
+  final Function onPress;
+
+  const MyButtonWidget(
+      {Key? key,
+      required this.text,
+      required this.color,
+      required this.onPress})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: MaterialButton(
+        onPressed: onPress(),
+        child:  Text(
+          text,
+          style: const TextStyle(
+            fontSize: 20,
+            color:  Colors.white,
+          ),
+        ),
+        height: 45,
+        elevation: 0,
+        color: color,
+        shape: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+            borderSide: BorderSide.none),
+      ),
+    );
+  }
+}
