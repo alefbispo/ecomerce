2e72a1a757b988fba85be5e9457880f9e81f6558
alefbispo <Blacksabbath21@>
2022-05-04 20:52:29 -0300

    criando padroes de style para o app

---
+++ ecomerce/lib/styles/login_screen_styles.dart
@@ -0,0 +1,12 @@
+import 'package:ecomerce/appColors/app_colors.dart';
+import 'package:flutter/cupertino.dart';
+
+class LoginScreenStyles {
+  static const TextStyle forgotPasswordStyles = TextStyle(
+    color: AppColors.baseDarkPinkColor,
+    fontSize: 18,
+  );
+  static const TextStyle singinSocialStyles = TextStyle(
+    color: AppColors.baseBlackColor,
+  );
+}
