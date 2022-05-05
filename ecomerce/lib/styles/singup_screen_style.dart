a3645419d989925e6d323369a9eac88b2a894af0
alefbispo <Blacksabbath21@>
2022-05-05 00:10:12 -0300

    layout da tela de singup finalizada

---
+++ ecomerce/lib/styles/singup_screen_style.dart
@@ -0,0 +1,33 @@
+import 'package:ecomerce/appColors/app_colors.dart';
+import 'package:flutter/cupertino.dart';
+
+class SingupScreenStyles {
+  // singInAgressStyle styles
+  static const TextStyle singInAgressStyle = TextStyle(
+    color: AppColors.baseBlackColor,
+    fontSize: 16,
+  );
+  // termsTextStyle styles
+  static const TextStyle termsTextStyle = TextStyle(
+    color: AppColors.baseDarkPinkColor,
+    fontSize: 16,
+  );
+  // andTextStyle styles
+  static const TextStyle andTextStyle = TextStyle(
+    fontSize: 16,
+  );
+  // conditionsTextStyletStyle styles
+  static const TextStyle conditionsTextStyle = TextStyle(
+    color: AppColors.baseDarkPinkColor,
+    fontSize: 16,
+  );
+  // singinSocialTextStyle styles
+  static const TextStyle singinSocialTextStyle = TextStyle(
+    color: AppColors.baseBlackColor,
+  );
+  // singupButtomTextStyle styles
+  static const TextStyle singupButtomTextStyle = TextStyle(
+    color: AppColors.baseBlackColor,
+    fontSize: 20,
+  );
+}
