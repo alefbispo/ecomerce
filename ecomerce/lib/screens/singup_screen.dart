b35ca502040aa3fb261fb6609c3d51ab9346da4f
alefbispo <Blacksabbath21@>
2022-05-04 22:32:46 -0300

    pagina singup

---
+++ ecomerce/lib/screens/singup_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SingupScreen extends StatelessWidget {
+  const SingupScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
