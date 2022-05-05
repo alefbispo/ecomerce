4a3a616664791636e9f0b3fd9fd39a0263298ab9
alefbispo <Blacksabbath21@>
2022-05-04 22:49:38 -0300

    compos de texto na sinup screen

--- ecomerce/lib/screens/singup_screen.dart
+++ ecomerce/lib/screens/singup_screen.dart
@@ -1,12 +1,39 @@
+import 'package:ecomerce/swvImages/swv_images.dart';
+import 'package:ecomerce/widgets/my_textfromfiel_widget.dart';
 import 'package:flutter/material.dart';
 
 class SingupScreen extends StatelessWidget {
-  const SingupScreen({ Key? key }) : super(key: key);
+  const SingupScreen({Key? key}) : super(key: key);
+
+  Widget buildTopPart() {
+    return Column(
+      children: [
+        Image.asset(
+          SvgImages.logo,
+          height: 150,
+        ),
+        MyTextFromField(
+          hintText: 'Full name',
+          obscureText: false,
+        ),
+        MyTextFromField(
+          hintText: 'Email',
+          obscureText: false,
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: SafeArea(
+          child: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          buildTopPart(),
+        ],
+      )),
     );
   }
 }
