40f8cfe9d40240139cc854158756841d86d0b971
alefbispo <Blacksabbath21@>
2022-05-04 21:08:40 -0300

    configurando a chamada das imagens atraves de uma classe

---
+++ ecomerce/lib/swvImages/swv_iages.dart
@@ -0,0 +1,16 @@
+class SvgImages {
+  String facebook = 'images/Facebook.svg';
+  String cart = 'images/cart.svg';
+  String cross = 'images/Cross.svg';
+  String download = 'images/Dowload.svg';
+  String edit = 'images/Edit.svg';
+  String google = 'images/Google.svg';
+  String heart = 'images/Heart.svg';
+  String layout = 'images/Layout.svg';
+  String plus = 'images/Plus.svg';
+  String search = 'images/Search.svg';
+  String setting = 'images/Setting.svg';
+  String trash = 'images/Trash.svg';
+  String twitter = 'images/Twitter.svg';
+  String upload = 'images/Upload.svg';
+}
