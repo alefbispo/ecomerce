9f1f22ab96b379f4d06378447a438ae6ee0700e9
alefbispo <Blacksabbath21@>
2022-05-11 23:20:50 -0300

    utilizando metodo tap bar nos itens do menu homepage

--- ecomerce/lib/screens/homepage/singup/tapbar_dada.dart
+++ ecomerce/lib/screens/homepage/singup/tapbar_dada.dart
@@ -1,24 +1,26 @@
+import 'package:ecomerce/models/single_product_model.dart';
 import 'package:flutter/material.dart';
 
-import '../../../data/home-page-data/home_page_data.dart';
 import '../../../widgets/single_product_widget.dart';
 
-class TabBatBar extends StatelessWidget {
-  const TabBatBar({Key? key}) : super(key: key);
+class TabBarBar extends StatelessWidget {
+  final List<SingleProductModel> productData;
+
+  const TabBarBar({Key? key, required this.productData}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      physics: NeverScrollableScrollPhysics(),
+      physics: const BouncingScrollPhysics(),
       shrinkWrap: true,
       primary: true,
-      itemCount: sigleProductData.length,
+      itemCount: productData.length,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 0.7,
       ),
       itemBuilder: (context, index) {
-        var data = sigleProductData[index];
+        var data = productData[index];
         return SingleProductWidget(
           onPressed: () {},
           productImage: data.productImage,
