e2d18d734381d4c783c4c20f05dbfa01ae618fcb
alefbispo <Blacksabbath21@>
2022-05-13 22:02:50 -0300

    bug

---
+++ ecomerce/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:ecomerce/appColors/app_colors.dart';
+import 'package:ecomerce/screens/longinScreen/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(
+            color: AppColors.baseBlackColor,
+          ),
+        ),
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
